a334c921a2a8aae0d885d02d1bef44609c97507f
Dan-Piatek <dan.piatek158@gmail.com>
2020-07-01 19:40:32 +0200

    JS changed into fuction

--- javascript/script.js
+++ javascript/script.js
@@ -1,14 +1,10 @@
-let plnElement = document.querySelector(".js-pln");
 let exchangeRateElement = document.querySelector(".js-exchangeRate");
 let formElement = document.querySelector(".js-form");
 let resultElement = document.querySelector(".js-result");
-let usdSign = document.querySelector(".js-usd");
-let euroSign = document.querySelector(".js-euro");
-let gbpSign = document.querySelector(".js-gbp");
+
 let currency = document.querySelector(".js-currency");
 let currencySign = document.querySelector(".js-currencySign");
 
-console.log(plnElement);
 console.log("witam");
 
 currency.addEventListener("input", () => {
@@ -23,16 +19,20 @@ currency.addEventListener("input", () => {
     }
 })
 
-formElement.addEventListener("submit", (event) => {
-    event.preventDefault();
-    console.log("Formularz został wysłany");
+function exchangeCalc() {
+    const plnElement = document.querySelector(".js-pln");
+    formElement.addEventListener("submit", (event) => {
+        event.preventDefault();
+        console.log("Formularz został wysłany");
 
-    let pln = plnElement.value;
-    let exchangeRate = exchangeRateElement.value;
+        const exchangeRate = exchangeRateElement.value;
 
-    let result = pln * exchangeRate;
-    result = result.toFixed(2);
-    console.log(result);
-    resultElement.innerText = result;
+        let result = plnElement.value * exchangeRate;
+        result = result.toFixed(2);
+        console.log(result);
+        resultElement.innerText = result;
 
-});
+    });
+}
+
+exchangeCalc();
